c5fe0e1d59591d3d1bb0d3c431c96455cb1fe879
Abhishek0086 <130195535+Abhishek0086@users.noreply.github.com>
2024-12-08 08:57:39 +0530

    format string

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 # Ask user for their name
-# name =input("what's your name?")
+name =input("what's your name?")
 
 # say hello to user
-# print('Hello ,' + name)
+print(f'Hello ,{name}')
 # print("hello, ", end="")
 # print(name)
 
 # Lets try to do it with sep
 # print("hello, ", name, sep="???")
-print("hello ,\"friend\"")
+# print("hello ,\"friend\"")
